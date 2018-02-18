ed3fba501953d23acef65fe882757c44d377df4a
Jordi <jordifusbel@hotmail.com>
2018-02-18 20:03:29 +0100

    Creado codigo para historia 5

---
+++ src/main/java/org/mvpigs/BlockChain.java
@@ -0,0 +1,29 @@
+package org.mvpigs;
+
+import java.util.ArrayList;
+
+public class BlockChain {
+    private ArrayList<Transaction> blockChain;
+
+    public BlockChain(){
+        this.blockChain = new ArrayList<Transaction>();
+    }
+
+    public ArrayList<Transaction> getBlockChain() {
+        return blockChain;
+    }
+
+    public void setBlockChain(ArrayList<Transaction> blockChain) {
+        this.blockChain = blockChain;
+    }
+
+    public void addOrigin(Transaction newBillet){
+        this.blockChain.add(newBillet);
+    }
+
+    public void summarize(){
+        for (int n=0; n< this.blockChain.size(); n++){
+            System.out.println(this.blockChain.get(n).toString());
+        }
+    }
+}
